8d9b0b9e239fbc26aa3ac3f287f4dbdcfe303812
Nuclear-Fox <grid30@mail.ru>
2023-12-26 21:03:36 +0300

    fix start

--- src/web/app.py
+++ src/web/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def get_word():
     return wordly_service.getWord(), 200
 
+@app.get("/")
+def start():
+    return render_template("wordly.html")
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
